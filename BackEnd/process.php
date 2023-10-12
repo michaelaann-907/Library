d077cbb4536e09989a21c62b45a050be30eb3af7
Michaela Pierce <114198365+michaelaann-907@users.noreply.github.com>
2023-10-11 23:37:20 -0400

    Create process.php

---
+++ BackEnd/process.php
@@ -0,0 +1,14 @@
+<?php
+// Connect to the database
+$host = "localhost"; // Change this to your MySQL server host
+$username = "your_username"; // Change this to your MySQL username
+$password = "your_password"; // Change this to your MySQL password
+$database = "your_database"; // Change this to your MySQL database name
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Need to add code for collecting and storing inputs to database
